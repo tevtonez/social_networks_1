428bfcaef8b5f8c32ab74c66fd60500cc40458c3
tevtonez <tevtonez@gmail.com>
2017-09-17 00:36:31 +0200

    fix issue with deleting the posts

--- posts/views.py
+++ posts/views.py
@@ -67,4 +67,4 @@ class DeletePost(LoginRequiredMixin, SelectRelatedMixin, generic.DeleteView):
 
     def delete(self, *args, **kwargs):
         messages.success(self.request, "Post Deleted")
-        return super(DeletePost, self).delete(request, *args, **kwargs)
+        return super().delete(*args, **kwargs)
